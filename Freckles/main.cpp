cfba71f8b9e29ebfb3fddf30f112e4b7789e6ca8
Zareek Galván <zareek99@gmail.com>
2015-10-29 22:50:06 -0600

    Prim algorithm kind of working for ACM-Freckles problem

--- Freckles/main.cpp
+++ Freckles/main.cpp
@@ -6,27 +6,27 @@
 //  Copyright © 2015 Trisquel Labs. All rights reserved.
 //
 
+#include <stdio.h>
+#include <limits.h>
 #include <iostream>
 #include <limits.h>
 #include <math.h>
 #include <iomanip>
 using namespace std;
 
-double f[100][100];
-double arrX[100];
-double arrY[100];
-int p[100];
+double graph[100][100];
+double arrX[100], arrY[100];
+double parent[100];
 int key[100];
-bool set[100];
+bool mstSet[100];
 
-double min(int n)
+double minKey(int key[], bool mstSet[], int n)
 {
-    double min = INT_MAX/4;
-    double min_index;
+    double min = INT_MAX/4, min_index;
     
     for (int v = 0; v < n; v++)
     {
-        if (set[v] == false && key[v] < min)
+        if (mstSet[v] == false && key[v] < min)
         {
             min = key[v];
             min_index = v;
@@ -36,88 +36,40 @@ double min(int n)
     return min_index;
 }
 
-void printMST(int n)
+double suma(int n)
 {
-    for (int i = 0; i < n; i++){
-        if(p[i] == 0)
-            cout << "A";
-        else if(p[i] == 1)
-            cout << "B";
-        else if(p[i] == 2)
-            cout << "C";
-        else if(p[i] == 3)
-            cout << "D";
-        else if(p[i] == 4)
-            cout << "E";
-        else if(p[i] == 5)
-            cout << "F";
-        else if(p[i] == 6)
-            cout << "G";
-        else if(p[i] == 7)
-            cout << "H";
-        
-        cout << " - ";
-        
-        if(i == 0)
-            cout << "A";
-        else if(i == 1)
-            cout << "B";
-        else if(i == 2)
-            cout << "C";
-        else if(i == 3)
-            cout << "D";
-        else if(i == 4)
-            cout << "E";
-        else if(i == 5)
-            cout << "F";
-        else if(i == 6)
-            cout << "G";
-        else if(i == 7)
-            cout << "H";
-        
-        cout << " has weight " << f[i][p[i]] << endl;
-        
+    double x;
+    for (int i = 1; i < n; i++)
+    {
+        x += graph[i][(int)parent[i]];
     }
+    return x;
 }
 
-void inicializa(int n)
+void primMST(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int  j = 0; j < n; j++)
-        {
-            f[i][j] = INT_MAX/4;
-        }
+        key[i] = INT_MAX/4, mstSet[i] = false;
     }
     
-    for (int i = 0; i < n; i++){
-        key[i] = INT_MAX/4;
-        set[i] = false;
-        f[i][i] = 0;
-        arrX[i] = 0;
-        arrY[i] = 0;
-    }
-}
-
-void Prim(int n)
-{
-    for (int count = 0; count < n; count++)
+    key[0] = 0;
+    parent[0] = -1;
+    
+    for (int count = 0; count < n-1; count++)
     {
-        int j = min(n);
+        int u = minKey(key, mstSet, n);
         
-        set[j] = true;
+        mstSet[u] = true;
         
-        for (int v = 0; v < n; v++){
-            
-            if (f[j][v] && set[v] == false && f[j][v] <  key[v]){
-                p[v]  = j;
-                key[v] = f[j][v];
+        for (int v = 0; v < n; v++)
+        {
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] <  key[v])
+            {
+                parent[v]  = u, key[v] = graph[u][v];
             }
         }
     }
-    
-    //printMST(n);
-    //return f[0][p[0]];
 }
 
 int main()
@@ -125,9 +77,6 @@ int main()
     int freckles;
     cin >> freckles;
     
-    inicializa(freckles);
-    
-    //Lee los datos y los pone en arreglos
     for (int i = 0; i < freckles; i++)
     {
         double x, y;
@@ -136,37 +85,22 @@ int main()
         arrY[i] = y;
         
     }
-    
-    //llena la matriz
     for (int i = 0; i < freckles; i++)
     {
         for (int j = i + 1; j < freckles; j++)
         {
             double valx = arrX[j] - arrX[i];
             double valy = arrY[j] - arrY[i];
-            double distance = sqrt(valx*valx + valy*valy);
-            f[i][j] = distance;
-            f[j][i] = distance;
+            double distance = hypot(valx, valy);
+            graph[i][j] = distance;
+            graph[j][i] = distance;
         }
     }
-    cout << endl;
-    for (int i = 0; i < freckles; i++)
-    {
-        for (int j = 0; j < freckles; j++)
-        {
-            cout << f[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-    Prim(freckles);
-    int line = freckles/2;
-    double acum;
-    for (int i = 0; i < freckles; i++)
-    {
-        acum += f[line][i];
-    }
-    cout<<fixed<<setprecision(2);
-    cout << acum << endl;
     
+    primMST(freckles);
+    
+    cout << fixed << setprecision(2);
+    cout << suma(freckles) << endl;
+    
+    return 0;
 }
